4b95294d90d6e77ae699db5ae2038a7e6f60518c
Miguel Ribeiro <miguel.g.ribeiro2007@gmail.com>
2026-05-17 17:54:42 +0100

    Update camera.gd

--- camera.gd
+++ camera.gd
@@ -3,7 +3,7 @@ extends Camera2D
 var is_panning = false
 
 func _input(event: InputEvent) -> void:
-	
+	var limite_posicao := 40
 	if event is InputEventMouseButton:
 		if event.pressed && event.button_index == MOUSE_BUTTON_LEFT:
 			is_panning = true
@@ -13,3 +13,4 @@ func _input(event: InputEvent) -> void:
 	if is_panning && event is InputEventMouseMotion:
 		var vector = event.relative
 		position -= vector
+		position = position.clamp(Vector2(-limite_posicao*48, -limite_posicao*48), Vector2(limite_posicao*48, limite_posicao*48))
