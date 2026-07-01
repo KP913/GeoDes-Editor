a2df739932dba5d57ea4c75f92373a1f7ec5ce4d
Miguel Ribeiro <miguel.g.ribeiro2007@gmail.com>
2026-07-01 16:30:23 +0100

    Update add_point_popup.gd and main.tscn

--- add_point_popup.gd
+++ add_point_popup.gd
@@ -1,11 +1,10 @@
 extends Control
-var valid_chars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ","]
+var valid_chars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ",", "-"]
 @onready var main = find_parent("Main")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -16,16 +15,15 @@ func add_char(char: String):
 		if $LineEdit.text.length() != 0:
 			$LineEdit.text = $LineEdit.text.substr(0, $LineEdit.text.length() - 1)
 	else:
+		#if char == "-"
+			#var text_temp = $LineEdit.text +char
+			
 		$LineEdit.text += char
-
 
 func _on_line_edit_text_changed(new_text: String) -> void:
 	for char in new_text:
 		if char not in valid_chars:
 			$LineEdit.text = new_text.remove_char(ord(char))
-			
-
-
 
 func close_popup() -> void:
 	$LineEdit.text = ""
@@ -51,4 +49,15 @@ func submit_text() -> void:
 	main.points.append(point)
 	main.add_point(point)
 	close_popup()
-	
+
+func check_valid_minus(coords: String):
+	var lista
+	lista = coords.split(",")
+	for coord in lista:
+		for i in coord.length():
+			if coord[i] == "-" and i != 0:
+				return false
+	return true
+				
+				
+				
